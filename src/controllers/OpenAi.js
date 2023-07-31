b8121437c2f876113dff58268c9b685fe212ff1a
Geoffrey Otieno <otienogeoffrey812@gmail.com>
2023-07-31 14:48:45 +0300

    Joi input validation

--- src/controllers/OpenAi.js
+++ src/controllers/OpenAi.js
@@ -1,8 +1,20 @@
 import Service from "../utils/Service.js";
 import { handleError } from '../utils/Function.js'
+import Joi from "joi";
 class OpenAI{
     static ChatCompletion = async(req, res)=>{
         try {
+            const schema = Joi.object({
+                message : Joi.string().required(),
+            })
+        
+            const result = schema.validate(req.body);
+        
+            if(result.error){
+                res.status(400).send(result.error.details[0].message);
+                return;
+            }
+
             await Service.openAIchatCompletion(req, res)
         } catch (error) {
             handleError(error, 500, res);
